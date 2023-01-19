6d75170594a968a5ba75df491ee6545c50aa6d56
niku6666 <120383717+niku6666@users.noreply.github.com>
2023-01-19 15:23:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// document.getElementById("laskuri-el").innerText = 5
+let laskuri= 0
+
+document.getElementById("Nappula")
+
+function IhmisenLisaus(){
+
+laskuri = laskuri + 1
+
+console.log(laskuri)
+
+document.getElementById("laskuri-el").innerText = laskuri
+
+}
+
+function IhmisenPoisto(){
+
+ laskuri = laskuri - 1
+
+console.log(laskuri)
+
+document.getElementById("laskuri-el").innerText = laskuri
+
+}
+
+
+
+
